879c0e48dd8d99071b75915651d1858acf6713c1
Ataberk Özyılmaz <ataberk279@gmail.com>
2024-02-28 19:04:19 +0300

    kategorilere göre postların listelenmesi eklendi.

--- blogs/admin.py
+++ blogs/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Category, Blog
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'category_slug': ('category_name',)}
+
+
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'category', 'author', 'status', 'is_featured')
@@ -9,6 +13,6 @@ class BlogAdmin(admin.ModelAdmin):
     list_editable = ('is_featured', 'status',)
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Blog, BlogAdmin)
 
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Blog
 
 # Create your views here.
+
+def posts_by_category(request, category_slug):
+    posts = Blog.objects.filter(status='Published', category__category_slug=category_slug)
+    category = get_object_or_404(Category, category_slug=category_slug)
+    context = {
+        'posts': posts,
+        'category': category
+    }
+    return render(request, 'posts_by_category.html', context)
