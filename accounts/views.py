3276ee99e04ddedceba12b44498e38761424e023
Ataberk Özyılmaz <ataberk279@gmail.com>
2024-03-12 20:07:05 +0300

    logout fonksiyonu eklendi.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 from accounts.forms import RegistrationForm
 
 
@@ -20,8 +22,25 @@ def register(request):
 
 
 def login(request):
-    context = {
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+
+    form = AuthenticationForm()
+    context = {
+        'form': form
     }
     return render(request, 'login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
 
