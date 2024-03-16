38b6d99d9c09a84b01404d0dc97e9e07f9c4508e
Ataberk Özyılmaz <ataberk279@gmail.com>
2024-03-16 16:15:18 +0300

    Dashboard uzerinde post CRUD eklendi.

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("categories/add", views.add_category, name='add_category'),
     path("categories/edit/<slug:category_slug>/", views.edit_category, name='edit_category'),
     path("categories/delete/<slug:category_slug>/", views.delete_category, name='delete_category'),
+    path("posts/", views.posts, name='posts'),
+    path("posts/add/", views.add_post, name='add_post'),
+    path("posts/edit/<slug:slug>/", views.edit_post, name='edit_post'),
+    path("posts/delete/<slug:slug>/", views.delete_post, name='delete_post'),
 ]
